e0f77884685e712930ae850c1a6d9c088b08f827
KatSchultz <ks.katieschultz@gmail.com>
2024-01-26 10:52:37 -0500

    routing and templates started

---
+++ src/App/Controllers/ProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+class ProfileController
+{
+    public function profile()
+    {
+        echo "This is the profile page";
+    }
+}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,7 +5,18 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
+use App\Controllers\ProfileController;
+
 
 $app = new App();
 
+$app->get('/', [HomeController::class, 'home']);
+$app->get('/profile', [ProfileController::class, 'profile']);
+
+
+
+// dd($app);
+
+// $app->get('/about/team');
 return $app;
