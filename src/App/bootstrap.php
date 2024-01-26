e712bbfcd1c055b259e54c50b96fb820dc453443
KatSchultz <ks.katieschultz@gmail.com>
2024-01-26 18:44:14 -0500

    completed template engine section

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,18 +5,11 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
-use App\Controllers\ProfileController;
+use function App\Config\registerRoutes;
 
 
 $app = new App();
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/profile', [ProfileController::class, 'profile']);
+registerRoutes($app);
 
-
-
-// dd($app);
-
-// $app->get('/about/team');
 return $app;
