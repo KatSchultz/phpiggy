9ee8be963616ef09c3886ea74904c0a2746338f7
KatSchultz <ks.katieschultz@gmail.com>
2024-01-28 18:23:14 -0500

    created rule and added to service, updated route to post form data

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\{HomeController, ProfileController, AboutController};
-// use App\Controllers\ProfileController;
-// use App\Controllers\AboutController;
+use App\Controllers\{HomeController, ProfileController, AboutController, AuthController};
 
 function registerRoutes(App $app)
 {
     $app->get('/', [HomeController::class, 'home']);
-    $app->get('/profile', [ProfileController::class, 'profile']);
     $app->get('/about', [AboutController::class, 'about']);
+    $app->get('/profile', [ProfileController::class, 'profile']);
+    $app->get('/register', [AuthController::class, 'registerView']);
+    $app->post('/register', [AuthController::class, 'register']);
 }
